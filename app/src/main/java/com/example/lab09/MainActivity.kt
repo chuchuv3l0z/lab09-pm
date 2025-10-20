b824458f76fe0d1d8a918cd6aee84c2cd7022f58
Thomas <chuchuv3l0z@gmail.com>
2025-10-19 23:21:50 -0500

    Eliminadas funciones de ejemplo Greeting y GreetingPreview de MainActivity

--- app/src/main/java/com/example/lab09/MainActivity.kt
+++ app/src/main/java/com/example/lab09/MainActivity.kt
@@ -41,30 +41,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab09Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ProgPrincipal9()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab09Theme {
-        Greeting("Android")
     }
 }
 
